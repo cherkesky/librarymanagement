6e46c6116625fc21f4dca4de517b66f0b3d4c5e6
Cherkesky <guy@cherkesky.com>
2020-02-05 17:21:46 -0600

    executed makemigration successfuly

--- libraryproject/libraryapp/models/book.py
+++ libraryproject/libraryapp/models/book.py
@@ -1,20 +1,22 @@
 from django.db import models
 from .library import Library
+from .librarian import Librarian
 
 class Book(models.Model):
-  title = model.models.CharField(max_length=50)
-  isbn = model.models.CharField(max_length=50)
-  author = model.models.CharField(max_length=50)
-  year = model.models.CharField(max_length=50)
+  title = models.CharField(max_length=50)
+  isbn = models.CharField(max_length=50)
+  author = models.CharField(max_length=50)
+  year = models.CharField(max_length=50)
 
   location = models.ForeignKey(Library, on_delete=models.CASCADE)
+  librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name = ("Book")
-        verbose_name_plural = ("Books")
+  class Meta:
+      verbose_name = ("Book")
+      verbose_name_plural = ("Books")
 
-    def __str__(self):
-        return self.name
+  def __str__(self):
+      return self.name
 
-    def get_absolute_url(self):
-        return reverse("Book_detail", kwargs={"pk": self.pk})
+  def get_absolute_url(self):
+      return reverse("Book_detail", kwargs={"pk": self.pk})
--- libraryproject/libraryapp/models/library.py
+++ libraryproject/libraryapp/models/library.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Library(models.Model):
-  title = model.models.CharField(max_length=50)
-  address = model.models.CharField(max_length=50)
+  title = models.CharField(max_length=50)
+  address = models.CharField(max_length=50)
     
-    class Meta:
-        verbose_name = ("Library")
-        verbose_name_plural = ("Librarys")
+  class Meta:
+      verbose_name = ("Library")
+      verbose_name_plural = ("Librarys")
 
-    def __str__(self):
-        return self.name
+  def __str__(self):
+      return self.name
 
-    def get_absolute_url(self):
-        return reverse("Library_detail", kwargs={"pk": self.pk})
+  def get_absolute_url(self):
+      return reverse("Library_detail", kwargs={"pk": self.pk})
