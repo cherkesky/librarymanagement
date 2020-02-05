10ec19c8a1a042b511f1356fba124d3dd6c4ab1a
Cherkesky <guy@cherkesky.com>
2020-02-05 17:09:29 -0600

    creating the librarian and adding location for foreign key for book

--- libraryproject/libraryapp/models/book.py
+++ libraryproject/libraryapp/models/book.py
@@ -1,10 +1,13 @@
 from django.db import models
+from .library import Library
 
 class Book(models.Model):
   title = model.models.CharField(max_length=50)
   isbn = model.models.CharField(max_length=50)
   author = model.models.CharField(max_length=50)
   year = model.models.CharField(max_length=50)
+
+  location = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("Book")
