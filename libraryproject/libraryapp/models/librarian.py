4a3055864f0811488a36d269952dc2be7504efe0
Cherkesky <guy@cherkesky.com>
2020-02-06 13:14:56 -0600

    created super user

--- libraryproject/libraryapp/models/librarian.py
+++ libraryproject/libraryapp/models/librarian.py
@@ -4,7 +4,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .library import Library
 
-
 class Librarian(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.ForeignKey(
